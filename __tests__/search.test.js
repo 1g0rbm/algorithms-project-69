011120beec422c340cdcd6c8e6e4b7d73e03997e
1g0rbm <m1g0rb89@gmail.com>
2025-08-09 10:43:06 +0300

    Added search by words.

---
+++ __tests__/search.test.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+import { expect, test } from 'vitest'
+import search from '../index.js'
+
+const doc1 = { id: 'doc1', text: 'I can\'t shoot straight unless I\'ve had a pint!' }
+const doc2 = { id: 'doc2', text: 'Don\'t shoot shoot shoot that thing at me.' }
+const doc3 = { id: 'doc3', text: 'I\'m your shooter.' }
+const docs = [doc1, doc2, doc3]
+
+test('search should return two results', () => {
+  expect(search(docs, 'shoot')).toEqual(['doc1', 'doc2'])
+})
+
+test('search should return empty resiult', () => {
+  expect(search(docs, 'scooter')).toEqual([])
+})
--- src/search.js
+++ src/search.js
@@ -1,3 +1,11 @@
-export default function search() {
-  return 'not found'
+/**
+ * @param {Array.<{id:string, text: string}>} docs
+ * @param {string} needle
+ *
+ * @returns {Array.<Object>}
+ */
+export default function search(docs, needle) {
+  return docs
+    .filter(doc => doc.text.split(' ').includes(needle))
+    .map(doc => doc.id)
 }
